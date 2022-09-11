479bc6336f5f22dfdbc2b67131a06896b7b1fc02
igapanovich <gapanovich.igor@gmail.com>
2022-09-11 12:26:21 -0500

    Enforce single instance

--- WinCommandPalette/Program.fs
+++ WinCommandPalette/Program.fs
@@ -1,46 +1,57 @@
 ﻿open System
+open System.Threading
 open System.Windows.Forms
 open WinCommandPalette.IO
 open WinCommandPalette.UI
 open WinCommandPalette.Logic
 
+[<Literal>]
+let SingleInstanceMutexName =
+    "WinCommandPalette.SingleInstance"
+
 [<EntryPoint; STAThread>]
 let main _ =
-    let commandConfig =
-        use configFileStream =
-            UserFiles
-                .getOrInitializeCommandConfigFileAsync()
+    use singleInstanceMutex =
+        new Mutex(false, SingleInstanceMutexName)
+
+    if not (singleInstanceMutex.WaitOne(1)) then
+        0
+    else
+        let commandConfig =
+            use configFileStream =
+                UserFiles
+                    .getOrInitializeCommandConfigFileAsync()
+                    .Result
+
+            CommandConfigParser
+                .parseAsync(
+                    configFileStream
+                )
                 .Result
 
-        CommandConfigParser
-            .parseAsync(
-                configFileStream
-            )
-            .Result
+        let viewModel =
+            { ViewModel.executeCommand =
+                fun handle ->
+                    let command =
+                        commandConfig.commands
+                        |> Seq.tryFind (fun c -> c.handle = handle)
 
-    let viewModel =
-        { ViewModel.executeCommand =
-            fun handle ->
-                let command =
-                    commandConfig.commands
-                    |> Seq.tryFind (fun c -> c.handle = handle)
+                    match command with // TODO report wrong command
+                    | Some command -> CommandExecutor.execute command.text
+                    | None -> () }
 
-                match command with // TODO report wrong command
-                | Some command -> CommandExecutor.execute command.text
-                | None -> () }
+        use windowInstanceManager =
+            InputWindow.createWindowInstanceManager ()
 
-    use windowInstanceManager =
-        InputWindow.createWindowInstanceManager ()
+        use keyboardHook =
+            KeyboardHook.create (fun () -> windowInstanceManager.ShowWindowSingleInstance viewModel)
 
-    use keyboardHook =
-        KeyboardHook.create (fun () -> windowInstanceManager.ShowWindowSingleInstance viewModel)
+        use notifyIcon =
+            NotificationAreaIcon.create ()
 
-    use notifyIcon =
-        NotificationAreaIcon.create ()
+        notifyIcon.Visible <- true
 
-    notifyIcon.Visible <- true
+        Application.Run()
 
-    Application.Run()
-
-    notifyIcon.Visible <- false
-    0
+        notifyIcon.Visible <- false
+        0
